1e3773188032ce9d3a4ea5ba891fbfdd5ea2fcc0
felipefln <felipecarvalho_22_@hotmail.com>
2019-09-24 12:05:04 -0300

    Feat: Create sample project react

---
+++ src/Generos.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+
+const Generos = () => {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios
+            .get('api/genres')
+            .then(res => {
+                setData(res.data.data)
+            })
+    }, [])
+
+    const renderizaLinha = record => {
+        return (
+            <tr key={record.key}>
+                <th scope="row">{record.id}</th>
+                <td>{record.name}</td>
+                <td><button>+</button></td>
+            </tr>
+        )
+    }
+
+    if (data.length === 0) {
+        return (
+            <div className='container'>
+                <h1>Genêros</h1>
+
+                <div className='alert alert-warning' role='alert'>
+                    Você não possui Genêros criados...
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div className='container'>
+            <h1>Genêros</h1>
+            <div>
+                <Link to='/generos/novo'>Novo Genêro</Link>
+            </div>
+
+            <table className="table table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Nome do Genêro</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(renderizaLinha)}
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default Generos
---
+++ src/Header.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { 
+  Navbar, 
+  NavbarBrand, 
+  Collapse, 
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} 
+  from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  const [open, setOpen ] =useState(false)
+  const toggle = () => {
+    setOpen(!open)
+  }
+  return (
+    <Navbar color='light' light expand='md'>
+        <NavbarBrand tag={Link} to='/'>Minhas Series</NavbarBrand>
+        <NavbarToggler onClick={toggle}/>
+          <Collapse isOpen={open} navbar>
+            <Nav className='ml-auto'>
+              <NavItem>
+                <NavLink href='/'>
+                  Genêros
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+      </Navbar>
+  )
+}
+
+export default Header;
